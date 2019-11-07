9b4a663ccc065bbe83e3424178a2054c032649fc
이유진 <jojoo66@naver.com>
2019-11-07 17:19:37 +0900

    asdf

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="root">
+    <header>header</header>
+    <nav>nav</nav>
+    <div>
+      <section>main</section>
+      <aside>aside</aside>
+    </div>
+    <footer>footer</footer>
+    </div>    
+  );
+}
+
+export default App;
